45dc97798840f99c018f382e97797f63754a9d47
derryev <derryev@ssfs.org>
2024-03-08 14:16:11 -0500

    working on clothing app

--- clothesApp/ContentView.swift
+++ clothesApp/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        SliderView()
 }
-
+ 
+    struct SliderView: View {
+        let images = ["image1", "image2", "image3", "image4"]
+        var body: some View {
+            //Text("Slider view")
+            VStack {
+                TabView {
+                    ForEach(0..<4) { i in
+                        Image("\(images[i])").resizable()
+                    }
+                }.tabViewStyle(PageTabViewStyle())
+            }
+        }
+    }
+    
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
 }
