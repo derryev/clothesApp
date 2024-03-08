af97d30b38790c6bb1a3ffdc1c2861330f68ce18
derryev <derryev@ssfs.org>
2024-03-08 13:30:34 -0500

    Initial Commit

---
+++ clothesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clothesApp
+//
+//  Created by Eva Derryberry on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ clothesApp/clothesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothesAppApp.swift
+//  clothesApp
+//
+//  Created by Eva Derryberry on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct clothesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
